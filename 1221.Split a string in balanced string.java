a3e68f0ee29168b2b8fd020f5f219a70b6fa56ec
deekshithapunuru-rgb <deekshithapunuru@gmail.com>
2026-07-27 13:51:31 +0530

    Create 1221.Split a string in balanced string.java

---
+++ 1221.Split a string in balanced string.java
@@ -0,0 +1,17 @@
+lass Solution {
+    public int balancedStringSplit(String s) {
+        int count = 0, balance = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == 'R')
+                balance++;
+            else
+                balance--;
+
+            if (balance == 0)
+                count++;
+        }
+
+        return count;
+    }
+}
