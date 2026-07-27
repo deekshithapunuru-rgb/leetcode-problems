02a62849b12d27be09e80de14f6532f05d49aaff
deekshithapunuru-rgb <deekshithapunuru@gmail.com>
2026-07-27 13:48:57 +0530

    Implement maxSubArray method for maximum sum

---
+++ 53.Maximum Subarray.java
@@ -0,0 +1,11 @@
+lass Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0, max = nums[0];
+        for (int n : nums) {
+            sum += n;
+            max = Math.max(max, sum);
+            if (sum < 0) sum = 0;
+        }
+        return max;
+    }
+}
