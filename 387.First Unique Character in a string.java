f19b449754f99e0a13ab6fc8f204399473c391c9
deekshithapunuru-rgb <deekshithapunuru@gmail.com>
2026-07-27 13:15:40 +0530

    Create 387.First Unique Character in a string.java

---
+++ 387.First Unique Character in a string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstUniqChar(String s) {
+       
+        int[] freq = new int[26];
+        char[] total= s.toCharArray();
+        int value=0;
+
+        for(char a:total)
+        {
+            freq[a-'a']++;
+        }
+    
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(freq[s.charAt(i)-'a']==1)
+             {
+                 return i;
+            }
+    }   
+     System.gc();
+    return -1;
+    
+ }
+}
